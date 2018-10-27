4dd3d21565171844e836dcf98d7242e472ce5c48
celineotter <celine.otter@gmail.com>
2018-10-26 21:27:24 -0700

    (CO) Nest landing screen tests

--- src/js/components/screens/Landing/__tests__/index.test.js
+++ src/js/components/screens/Landing/__tests__/index.test.js
@@ -3,42 +3,44 @@ import { getShallowProviderAndWrapper, getInitializedConnectedComponent } from '
 
 import Landing from '@screens/Landing';
 
-describe('when user type is not set in the redux store', () => {
-  it('renders landing text with no traffic light, ', () => {
-    const landing = getShallowProviderAndWrapper(<Landing />).wrapper;
+describe('Landing screen', () => {
+  describe('when user type is not set in the redux store', () => {
+    it('renders landing text with no traffic light, ', () => {
+      const landing = getShallowProviderAndWrapper(<Landing />).wrapper;
 
-    expect(landing).toIncludeText('Ancestry Coding Challenge');
-    expect(landing.find({ testid: 'intro' })).toHaveText('Traffic Light Exercise!');
+      expect(landing).toIncludeText('Ancestry Coding Challenge');
+      expect(landing.find({ testid: 'intro' })).toHaveText('Traffic Light Exercise!');
 
-    expect(landing.find('TrafficLight').exists()).toEqual(false);
-    expect(landing).toMatchSnapshot();
-  });
-});
-
-describe('when user is of type "production" in the redux store', () => {
-  let wrapper;
-
-  beforeEach(() => {
-    wrapper = getInitializedConnectedComponent(<Landing />, { auth: { userType: 'production' } });
+      expect(landing.find('TrafficLight').exists()).toEqual(false);
+      expect(landing).toMatchSnapshot();
+    });
   });
 
-  it('renders traffic light without set color props ', () => {
-    const colorProps = wrapper.find('TrafficLight').props().colors;
-    expect(colorProps).toBe(null);
-    expect(wrapper).toMatchSnapshot();
+  describe('when user is of type "production" in the redux store', () => {
+    let wrapper;
+
+    beforeEach(() => {
+      wrapper = getInitializedConnectedComponent(<Landing />, { auth: { userType: 'production' } });
+    });
+
+    it('renders traffic light without set color props ', () => {
+      const colorProps = wrapper.find('TrafficLight').props().colors;
+      expect(colorProps).toBe(null);
+      expect(wrapper).toMatchSnapshot();
+    });
   });
-});
 
-describe('when user is of type "pilot" in the redux store', () => {
-  let wrapper;
+  describe('when user is of type "pilot" in the redux store', () => {
+    let wrapper;
 
-  beforeEach(() => {
-    wrapper = getInitializedConnectedComponent(<Landing />, { auth: { userType: 'pilot' } });
-  });
+    beforeEach(() => {
+      wrapper = getInitializedConnectedComponent(<Landing />, { auth: { userType: 'pilot' } });
+    });
 
-  it('renders traffic light with set color props ', () => {
-    const colorProps = wrapper.find('TrafficLight').props().colors;
-    expect(colorProps).toEqual([ 'blue', 'orange', 'purple' ]);
-    expect(wrapper).toMatchSnapshot();
+    it('renders traffic light with set color props ', () => {
+      const colorProps = wrapper.find('TrafficLight').props().colors;
+      expect(colorProps).toEqual([ 'blue', 'orange', 'purple' ]);
+      expect(wrapper).toMatchSnapshot();
+    });
   });
 });
